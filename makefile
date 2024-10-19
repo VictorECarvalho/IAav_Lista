e79f11458a42f69c08b0aaa40e72567e9b9ff95d
VictorECarvalho <victorelitt@gmail.com>
2024-10-19 20:24:10 -0300

    parsign entrance

---
+++ makefile
@@ -0,0 +1,14 @@
+# My first makefile
+
+
+all: main 
+ 
+ 
+main: src/lista1.cpp src/Search.h src/Search.cpp
+	g++ -o main src/lista1.cpp src/Search.h src/Search.cpp src/Estate.h src/Estate.cpp
+
+clean: 
+	rm main 
+
+run:./main
+	./main -gbfs 0 6 1 7 4 2 3 8 5, 5 0 2 6 4 8 1 7 3, 2 4 7 0 3 6 8 1 5
---
+++ src/Search.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include "Estate.h"
+using namespace std;
+
+class Search {
+    public:     
+        string algorithm;
+        list <string> open;
+        list <string> closed;  
+        Search(){
+            algorithm = "";
+        }
+};
